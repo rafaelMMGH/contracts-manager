fc07268da9e126da59488a87a0e7557dbb9c0b4f
Rafael Alberto Martinez Mendez <rmartinez@capi.lat>
2023-01-02 06:21:39 -0600

    update and format components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, {useRef} from 'react';
+import AddressComponent from "./components/addressComponent";
+import ContractTypeComponent from "./components/contractTypeComponent";
 
 function App() {
   const firstSection  = useRef(null);
@@ -9,57 +11,16 @@ function App() {
   return (
     <div className="bg-slate-900 ">
 
+
       <div className="flex flex-col rounded-3xl px-6 sm:px-8 order-first py-8 lg:order-none h-screen place-content-center" ref={firstSection}>
         <div className="md:grid md:grid-cols-6 md:gap-6">
           <div className="mt-5 md:col-span-4 md:col-start-2 md:mt-0">
             <form action="#" method="POST">
               <div className="shadow sm:overflow-hidden sm:rounded-md">
                 <div className="space-y-6 bg-gray-100 px-4 py-5 sm:p-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <legend className="contents text-base font-medium text-gray-900">Tipo de contrato</legend>
-                      <div className="mt-4 space-y-4">
-                        <div className="flex items-center">
-                          <input
-                            id="push-everything"
-                            name="push-notifications"
-                            type="radio"
-                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                          />
-                          <label htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
-                            Casa Habitación
-                          </label>
-                        </div>
-                        <div className="flex items-center">
-                          <input
-                            id="push-email"
-                            name="push-notifications"
-                            type="radio"
-                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                          />
-                          <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
-                            Comercio
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-                      Dirección
-                    </label>
-                    <div className="mt-1">
-                      <select
-                        id="address"
-                        name="address"
-                        autoComplete="address-name"
-                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white text-black py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>Mexico</option>
-                      </select>
-                    </div>
-                  </div>
+                    <ContractTypeComponent></ContractTypeComponent>
+
+                    <AddressComponent></AddressComponent>
                 </div>
                 <div className="bg-blue-700 px-4 py-3 text-right sm:px-6">
                   <button
@@ -88,9 +49,9 @@ function App() {
         <button type="button"
                 onClick={() => scrollTo(firstSection)}
                 className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                stroke="currentColor" className="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"/>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"/>
           </svg>
 
           <span className="sr-only">Icon description</span>
@@ -196,7 +157,7 @@ function App() {
 
                     <div className="col-span-6 sm:col-span-6 lg:col-span-2 lg:col-start-3">
                       <label htmlFor="sign-date" className="block text-sm font-medium text-gray-700">
-                        Fecha de firma
+                        Fecha de firma de contrato
                       </label>
                       <input
                         type="date"
